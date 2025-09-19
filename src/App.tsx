a47fce5260d0815fbe0d40ce7edfc627a5f89f1a
danielkerwin <dkerwin@me.com>
2025-09-19 11:03:15 +1000

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,64 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { toDocx } from 'mdast2docx';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
+import React from 'react';
+import { marked } from "marked";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const markdown = `
+    # Sample Document  
+    This is a **bold** text and _italic_ text.  
+
+    > A blockquote example  
+
+    - List Item 1[^1]  
+    - List Item 2[^2]  
+
+    [Click Here](https://example.com)  
+
+    This is a footnote reference.  
+
+    [^1]: This is the first footnote content.
+    [^2]: This is the second footnote content.
+  `;
+  const [parsed, setParsed] = useState('')
+
+  const parse = async () => {
+    const md = await marked(markdown)
+    setParsed(md);
+  }
+
+  const mdast = unified().use(remarkParse).parse(markdown);
+
+  const downloadDocX = async () => {
+    const docxBlob = await toDocx(mdast, { title: "My Document" }, {});
+    const url = URL.createObjectURL(docxBlob as Blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "document.docx";
+    link.click();
+    URL.revokeObjectURL(url);
+  }
+
+  useEffect(() => {
+    parse();
+  })
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>mdast2docx footnote issue</h1>
+      <div className="card" dangerouslySetInnerHTML={{ __html: parsed }}>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={downloadDocX}>
+          Convert MD to Docx
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
